b7e0249f4191bbba77c1f9723bbe20c4febd621b
sendres1 <sendres1@L11104042784.wctc.msft>
2014-10-01 17:24:58 -0500

    class lab

--- src/discount/strategy/QuantityDiscount.java
+++ src/discount/strategy/QuantityDiscount.java
@@ -3,44 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package discount.strategy;
 
 /**
  *
  * @author sendres1
  */
-public  class QuantityDiscount implements DiscountStrategy {
-    
-
+public class QuantityDiscount implements DiscountStrategy {
 
     // private values =[5,10,20,100];
-    
-
-
-    
     @Override
-    public double calcDiscountAmt(LineItem item){
+    public double calcDiscountAmt(LineItem item) {
         double discountAmt;
         int quantity = item.getQty();
         double unitPrice = item.getProduct().getUnitPrice();
-        
-//        switch (month) {
-//            case 1:  monthString = "January";
-//                     break;
-//            case 2:  monthString = "February";
-        
-        if (quantity > 100)
-                discountAmt = quantity * unitPrice * .10;
-        else   if (quantity > 20)
-                discountAmt = quantity * unitPrice * .05; 
-                else discountAmt = 0;
-        
-        return discountAmt;
-        
-    }
-        
-    
-}
+        int FIRST_QTY_BREAK = 20;
+        double FIRST_QTY_DISCOUNT = .05;
+        int SECOND_QTY_BREAK = 100;
+        double SECOND_QTY_DISCOUNT = .10;
 
-    
+        if (quantity > SECOND_QTY_BREAK) {
+            discountAmt = quantity * unitPrice * SECOND_QTY_DISCOUNT;
+        } else if (quantity > FIRST_QTY_BREAK) {
+            discountAmt = quantity * unitPrice * FIRST_QTY_DISCOUNT;
+        } else {
+            discountAmt = 0;
+        }
+
+        return discountAmt;
+
+    }
+
+}
--- src/discount/strategy/Startup.java
+++ src/discount/strategy/Startup.java
@@ -4,10 +4,11 @@ import java.text.NumberFormat;
 import java.util.Arrays;
 
 /**
- * Just a start and test class for this program. You may modify this class
- * in any way you see fit.
+ * Just a start and test class for this program. You may modify this class in
+ * any way you see fit.
  *
  * @author sendres1
+ * @version 1.100
  */
 public class Startup {
 
@@ -17,45 +18,21 @@ public class Startup {
 
         Store s = new Store(9102, "Kohls Brookfield Store");
         Customer c = fd.getCustomerByID(1); //new Customer(1,"Endres" );
+
+        POSRegister reg = new POSRegister(c, s);
+        reg.newReceipt();
+        reg.additem(fd.getProductByID("A"), 100);
+        reg.additem(fd.getProductByID("B"), 99);
+        reg.endReceipt();
+
         
-       // System.out.print(s.)
-        
-        
-       // this doesn't work'
-      //  System.out.println(Arrays.toString(fd.getCustomerArray()));
-       // System.out.println(fd.toString());
-       // System.out.println(fd.customerArray[0]);
-        //customerArray[1])
-              
-        //this works
-   //      Receipt r = new Receipt("jacket", 1, 23.00); 
-    //     System.out.println(r.getDescription());
-         // productID, qty, customerno, discount
-        
-         POSRegister reg = new POSRegister(c,s);
-         reg.newReceipt();
-         reg.additem(fd.getProductByID("A"), 100);
-         reg.additem(fd.getProductByID("B"), 99);
-      //   sale 1
-      //   sale 2
-      //   end sale
-         
-         reg.endReceipt();
-         
-          Customer c2 = fd.getCustomerByID(2); //new Customer(1,"Endres" );
-         POSRegister reg2 = new POSRegister(c,s);
-         reg2.newReceipt();
-         reg2.additem(fd.getProductByID("B"), 100);
-         reg2.additem(fd.getProductByID("A"), 99);
-      //   sale 1
-      //   sale 2
-      //   end sale
-         
-         reg2.endReceipt();
-         
-        // Receipt sale = reg.
-        
-        // System.out.println(reg.getCustomerno());
+        Customer c2 = fd.getCustomerByID(2); //new Customer(1,"Endres" );
+        POSRegister reg2 = new POSRegister(c2, s);
+        reg2.newReceipt();
+        reg2.additem(fd.getProductByID("B"), 100);
+        reg2.additem(fd.getProductByID("A"), 99);
+        reg2.endReceipt();
+
     }
 //
 }
